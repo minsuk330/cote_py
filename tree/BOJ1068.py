fb263fd2f8e9ec97018a8a9a0c713af2cfa239f9
watoo <minsuk33011@gmail.com>
2026-01-16 23:14:00 +0900

    1068:dfs 풀이

--- tree/BOJ1068.py
+++ tree/BOJ1068.py
@@ -3,46 +3,40 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
+
 node_count = int(input())
 
-def delete(node):
-    if node is None or node not in tree:
-        return
-    if tree[node]==[]:
-        del tree[node]
-        return
-    if len(tree[node]) == 2:
-        left, right = tree[node]
-    elif len(tree[node]) == 1:
-        left = tree[node][0]
-        right = None
-    else:
-        left = right = None
-    delete(left)
-    delete(right)
-    
 numbers = list(map(int,input().split()))
-tree = {i:[] for i in range(node_count)}
-for i in range(len(numbers)):
-    #루트 노드 처리
-    parent = numbers[i]
-    if parent!=-1:
-        tree[parent].append(i)
 
 node_delete = int(input())
-delete(node_delete)
 
-if node_delete == -1:
-    print(0)
-else:
-    delete(node_delete)
-    if numbers[node_delete] != -1:
-        parent = numbers[node_delete]
-        if parent in tree and node_delete in tree[parent]:
-            tree[parent].remove(node_delete)
+tree = [[] for _ in range(node_count)]
+root = -1
+
+for i in range(node_count):
+    parent = numbers[i]
+    if parent == -1:
+        root = i
+    else:
+        tree[parent].append(i)
+
+#index가 노드
+#리스트의 값이 부모
+def count_node(node):
+
+    if node == node_delete:
+        return 0
+    
+    if not tree[node]:
+        return 1
 
     count = 0
-    for key, value in tree.items():
-        if value == []:
-            count += 1
-    print(count)
+    for child in tree[node]:
+        count += count_node(child)
+
+    return count if count>0 else 1
+
+if root == node_delete:
+    print(0)
+else:
+    print(count_node(root))
