cb15d1d4c308f76318c4d8da9590dd0f5c2d2aa6
watoo <minsuk33011@gmail.com>
2026-02-25 20:22:17 +0900

    solve: 15654 solve1.py

---
+++ baekjoon/백트래킹/15654/solve1.py
@@ -0,0 +1,25 @@
+import sys
+def input():
+  return sys.stdin.readline().rstrip()
+
+N,M = map(int,input().split())
+numbers = list(map(int,input().split()))
+numbers.sort()
+visited = [False]*(N+1)
+result = []
+#1~N까지의 수열 같은수를 여러번 골라도 된다. 증가하는 순으로 작성
+
+def backtrack(depth):
+  if depth==N or len(result)==M:
+    print(' '.join(map(str,result)))
+    return
+  
+  for i,num in enumerate(numbers):
+    if not visited[i]:
+      visited[i] = True
+      result.append(num)
+      backtrack(depth+1)
+      visited[i] = False
+      result.pop()
+
+backtrack(0)
